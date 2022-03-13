c9c7be91545c8ea4158b3183e363d5a7decf0f22
sinoptik13rus <set.91@mail.ru>
2022-03-13 18:48:04 +0300

    3 zadanie

--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -1,19 +1,25 @@
 fun main() {
-    val summInRub = 10000
-    val kopInRub = 100
-    val amount = summInRub * kopInRub
-    val minComissioninRub = 35
-    val minComissionInCop = minComissioninRub * kopInRub
-    val comissionPercent = 0.75
-    val percentSumm = amount / 100 * comissionPercent
+    val regularCustomer = true
+    val amountPurchase = 17500
 
-    if (percentSumm < minComissionInCop) {
-        println("Total comission from the amount of $summInRub was $minComissionInCop kop.")
-        println("Total comission from the amount of $summInRub was $minComissioninRub rub.")
-    } else {
-        println("Total comission from the amount of $summInRub was $percentSumm kop.")
-        println("Total comission from the amount of $summInRub was ${percentSumm / 100} rub.")
-    }
+    val discount = 0
+    val regularDiscount = amountPurchase/100
+    val discount1 = 100
+    val regularDiscount1 = (amountPurchase - discount1)/100
+    val discount2 = (amountPurchase/100)*5
+    val regularDiscount2 = (amountPurchase - discount2)/100
 
+
+    if (amountPurchase >= 0 && amountPurchase <= 1000) {
+        if (regularCustomer === false) {
+            println("Сумма к оплате: ${amountPurchase - discount} руб. Скидка составила: $discount руб.")
+        } else println("Сумма к оплате: ${amountPurchase - discount - regularDiscount} руб. Скидка составила: ${discount + regularDiscount} руб.")
+    } else if (amountPurchase >= 1001 && amountPurchase <= 10000) {
+        if (regularCustomer === false) {
+            println("Сумма к оплате: ${amountPurchase - discount1} руб. Скидка составила: $discount1")
+        } else println("Сумма к оплате: ${(amountPurchase - discount1) - regularDiscount1} руб. Скидка составила: ${discount1 + regularDiscount1} руб.")
+    } else if (regularCustomer === false) {
+        println("Сумма к оплате: ${amountPurchase - discount2} руб. Скидка составила: $discount2")
+    } else println("Сумма к оплате: ${(amountPurchase - discount2) - regularDiscount2} руб. Скидка составила: ${discount2 + regularDiscount2} руб.")
 
 }
