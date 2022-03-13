3345062190d2cbec953e0e55210d13f265fce3f4
sinoptik13rus <set.91@mail.ru>
2022-03-13 18:46:59 +0300

    2 zadanie

--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -1,19 +1,10 @@
 fun main() {
-    val summInRub = 10000
-    val kopInRub = 100
-    val amount = summInRub * kopInRub
-    val minComissioninRub = 35
-    val minComissionInCop = minComissioninRub * kopInRub
-    val comissionPercent = 0.75
-    val percentSumm = amount / 100 * comissionPercent
+    val likesNumber = 105
+    val remainder = likesNumber % 10
 
-    if (percentSumm < minComissionInCop) {
-        println("Total comission from the amount of $summInRub was $minComissionInCop kop.")
-        println("Total comission from the amount of $summInRub was $minComissioninRub rub.")
+    if (remainder == 1) {
+        println("Понравилось $likesNumber человеку")
     } else {
-        println("Total comission from the amount of $summInRub was $percentSumm kop.")
-        println("Total comission from the amount of $summInRub was ${percentSumm / 100} rub.")
+        println("Понравилось $likesNumber людям")
     }
-
-
 }
